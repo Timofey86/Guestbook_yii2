aad453a5dafb0fa65e1ac703e0e8d75b17984f7f
Tim Mukhin <MuhinTV@gmail.com>
2022-08-06 09:18:52 +0700

    add Likes - not correct working add likes component

---
+++ modules/admin/models/Like.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "like".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $feedback_id
+ *
+ * @property Feedback $feedback
+ * @property Users $user
+ */
+class Like extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'like';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'feedback_id'], 'required'],
+            [['user_id', 'feedback_id'], 'integer'],
+            [['feedback_id'], 'exist', 'skipOnError' => true, 'targetClass' => Feedback::className(), 'targetAttribute' => ['feedback_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'feedback_id' => Yii::t('app', 'Feedback ID'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Feedback]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFeedback()
+    {
+        return $this->hasOne(Feedback::className(), ['id' => 'feedback_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'user_id']);
+    }
+}
